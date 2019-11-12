2222cfe6868f98b53633ae36f2641f781e2e265e
Anthony Freda <anthonyfreda323@gmail.com>
2019-11-12 12:55:01 -0500

    remove log

--- test/components/nav.test.tsx
+++ test/components/nav.test.tsx
@@ -5,7 +5,6 @@ import Nav from '../../components/Nav'
 describe('/components/nav.tsx', () => {
   it('Should render both links', () => {
     const nav = render(<Nav />)
-    console.log(nav.baseElement.querySelector('ul').childNodes)
     expect(nav.baseElement.querySelector('ul').childNodes).toHaveLength(2)
   })
 })
